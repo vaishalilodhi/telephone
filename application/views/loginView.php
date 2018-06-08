5ad962c12874d4a684ca2d9c2b02f3af45308260
vaishali lodhi <vaishali.lodhi@sts.in>
2018-06-08 18:47:16 +0530

    test_two

--- application/views/loginView.php
+++ application/views/loginView.php
@@ -1,31 +1,31 @@
 <?php
-require_once( dirname( __FILE__ ) . '/include/header.php');
 require_once( dirname( __FILE__ ) . '/include/navbar.php');
 ?>
-<div class="row">
-    <?php
-    $success_msg = $this->session->flashdata('success_msg');
-    $error_msg = $this->session->flashdata('error_msg');
-    if (isset($success_msg)) { ?>
-        <div class="alert alert-success"><?=$success_msg;?></div>
-    <?php } elseif(isset($error_msg)) { ?>
-        <div class="alert alert-danger"><?=$error_msg;?></div>
-    <?php } ?>
-    <form class="col-md-4" method="post" action="<?=base_url('login/login_user');?>">
-        <div class="form-group">
-            <label for="user_name">User Name</label>
-            <input type="text" class="form-control" id="user_name" name="user_name" placeholder="Enter User Name">
-        </div>
-        <div class="form-group">
-            <label for="user_password">Password</label>
-            <input type="password" class="form-control" id="user_password" name="user_password" placeholder="Password">
-        </div>
-        <input type="submit" value="Login" name="login" class="btn btn-primary" />
-        <button type="submit" class="btn btn-danger">Cancel</button>
-    </form>
-</div>
-<br>
+
 <div class="container">
+    <div class="row">
+        <?php
+        $success_msg = $this->session->flashdata('success_msg');
+        $error_msg = $this->session->flashdata('error_msg');
+        if (isset($success_msg)) { ?>
+            <div class="alert alert-success"><?=$success_msg;?></div>
+        <?php } elseif(isset($error_msg)) { ?>
+            <div class="alert alert-danger"><?=$error_msg;?></div>
+        <?php } ?>
+        <form class="col-md-4" method="post" action="<?=base_url('login/login_user');?>">
+            <div class="form-group">
+                <label for="user_name">User Name</label>
+                <input type="text" class="form-control" id="user_name" name="user_name" placeholder="Enter User Name">
+            </div>
+            <div class="form-group">
+                <label for="user_password">Password</label>
+                <input type="password" class="form-control" id="user_password" name="user_password" placeholder="Password">
+            </div>
+            <input type="submit" value="Login" name="login" class="btn btn-primary" />
+            <button type="submit" class="btn btn-danger">Cancel</button>
+        </form>
+    </div>
+    <br>
     <div class="row">
         <p>New user <a href="<?=base_url('login/register');?>">Register here.</a></p>
     </div>
--- application/views/user_profile.php
+++ application/views/user_profile.php
@@ -1,3 +1,37 @@
 <?php
-require_once( dirname( __FILE__ ) . '/include/header.php');
+require_once( dirname( __FILE__ ) . '/include/navbar.php');
 ?>
+<div class="container">
+    <div class="dropdown">
+        <button class="btn btn-primary dropdown-toggle pull-right" type="button" data-toggle="dropdown">Click to Sort<span class="caret"></span></button>
+        <ul class="dropdown-menu">
+            <li><a href="#">Name</a></li>
+            <li><a href="#">Date</a></li>
+        </ul>
+    </div>
+    <div class="row">
+    <table class="table table-hover">
+        <tr>
+            <th>User Name</th>
+            <th>First Name</th>
+            <th>Middle Name</th>
+            <th>Last Name</th>
+            <th>Email Id</th>
+            <th>Mobile Number</th>
+            <th>Landline</th>
+        </tr>
+        <!-- <?php 
+        foreach ($variable as $key => $value) {
+
+        } ?> -->
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </div>
+</div>
